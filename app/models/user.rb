5e87c80f2e416b61ff344a2309e6c9461b2be14b
inga mahle <inga.mahle@gmx.de>
2019-05-06 17:12:44 +0300

    Finish user edit, update, index and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX},
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -27,8 +27,6 @@ end
 
   def authenticated?(remember_token)
     return false if remember_digest.nil?
-    puts 'Hello from authenticated'
-    puts remember_digest
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
