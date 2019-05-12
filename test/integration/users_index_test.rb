40ef4840682fa980782bb8f3813d201519d29987
inga mahle <inga.mahle@gmx.de>
2019-05-12 07:54:26 +0300

    Add password reset

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -20,10 +20,22 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     end
     assert_difference'User.count', -1 do
       delete user_path(@non_admin)
+    end
+  end
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "index does not show users that are not activated and their page is not accessible" do
+    log_in_as(@admin)
+    @non_admin.toggle!(:activated)
+    assert_not_equal @non_admin.activated?, true
+    get users_path
+    assert_select 'a[href=?]', user_path(@non_admin), false
+    get user_path(@non_admin)
+    assert_redirected_to root_url
   end
 end
-
-test "index as non-admin" do log_in_as(@non_admin)
-get users_path
-assert_select 'a', text: 'delete', count: 0
-end end
